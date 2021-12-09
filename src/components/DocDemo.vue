3d58387acc5430fca372c1e84a76514122c29fdd
HerryZheng <303983522@qq.com>
2021-12-09 20:21:37 +0800

    switch css 1

---
+++ src/components/DocDemo.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>doc</p>
+
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SwitchDemo"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <p>tabs的文档</p>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SwitchDemo"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
