03d41233065f9ff3e403807c30bf680dd5c489a5
HerryZheng <303983522@qq.com>
2021-12-22 21:35:56 +0800

    tab 选中横线获取标签宽度

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,7 +2,7 @@
 <div>
   <p>tabs的文档</p>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -12,10 +12,16 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import {ref} from "vue";
+
 export default {
 components:{
   Tabs,Tab
-}
+},
+  setup(){
+  const x = ref('导航1')
+    return{x}
+  }
 };
 </script>
 
