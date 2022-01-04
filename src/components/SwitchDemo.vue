f9b71d9e6f4031de6fcf183c0f0e88ec115c7097
HerryZheng <303983522@qq.com>
2022-01-04 21:30:43 +0800

    展示源代码并高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,25 +4,25 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo/>
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')"/>
       </div>
     </div>
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo/>
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"/>
       </div>
     </div>
   </div>
@@ -33,12 +33,15 @@ import Button from "../lib/Button.vue";
 import {ref} from "vue";
 import Switch1Demo from "./Switch1.demo.vue"
 import Switch2Demo from "./Switch2.demo.vue"
+import 'prismjs'
+import 'prismjs/themes/prism.css'
+const Prism = (window as any).Prism
 
 export default {
-  components: {Switch1Demo,Switch2Demo, Switch, Button},
+  components: {Switch, Button},
   setup() {
     const bool = ref(false);
-    return {bool};
+    return {bool,Switch1Demo,Switch2Demo,Prism};
   }
 };
 </script>
